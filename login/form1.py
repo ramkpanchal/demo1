5d55b690bee034ca1cd541c4dd2c92ee175a733b
ramkpanchal <ramkpanchal@gmail.com>
2016-06-21 14:32:49 -0700

    first commit

---
+++ login/form1.py
@@ -0,0 +1,24 @@
+import re
+from django impor forms 
+from django.contrib.auth.models import User
+from django.utils.translation import ugettext_lazy as_
+
+class RegistrationForm(forms.Form):
+username = forms.RegexField(regex=r'^\w+$', widget=forms.TextInput(attrs=dict(required=true, max_length = 30)), lable= _("username"), error_messages ={'invalid'("please enter username.")} )
+Email= forms.EmailField(widget=forms.TextInput(attrs=dict(required=true, max_length = 30)), lable= _("EmailId") )
+password1= forms.CharField(widget=forms.TextInput(attrs=dict(required=true, max_length = 30, render_value = False)), lable= _("password1") )
+password2= forms.CharField(widget=forms.TextInput(attrs=dict(required=true, max_length = 30, render_value = False)), lable= _("password2") )
+
+
+def clean_username(self):
+	try:
+		user = user.objects.get(username_iexact= self.cleaned_data['username'])
+	except User.DoesNotExist:
+		return self.cleaned_data['username']
+		raise forms.ValidationsError(_("The username already exists/ please try another one"))
+
+def clean(self): 
+	if 'password' in self.clean_data and 'password' in self.cleaned_data:
+if self.clean_data['password'] != self.cleaned_data['password2':
+	raise forms.ValidationError(_("the two password fields did not match"))]
+	return self/cleaned_data
