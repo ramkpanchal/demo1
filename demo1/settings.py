a9c21acebe61a292a3580683b49cfef5b9f344cd
ramkpanchal <ramkpanchal@gmail.com>
2016-06-25 17:44:09 -0700

    changes

--- demo1/settings.py
+++ demo1/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'demo1.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [('C:\\Python27\\Lib\\site-packages\\django\\contrib\admin\\templates\\admin')],
+        'DIRS': [('C:\Python27\Lib\site-packages\django\contribadmin\templates\admin')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
